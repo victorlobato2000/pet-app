c8195c36a52f42226a8ae4b01c3db9930973c394
victorlobato2000 <victorsilva123190@gmail.com>
2021-11-26 12:59:38 -0300

    Adicionando back-end com dependências necessárias

---
+++ back-end/src/main/java/com/victorlobato/backendpet/BackendPetApplication.java
@@ -0,0 +1,13 @@
+package com.victorlobato.backendpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPetApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/victorlobato/backendpet/BackendPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorlobato.backendpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
